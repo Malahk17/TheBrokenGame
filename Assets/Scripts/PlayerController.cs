cb49b157d9237f64d1b02fb6f9d685b25b79294a
Paolo Malacarne <paolo.malacarne@studenti.unitn.it>
2020-02-01 03:54:11 +0100

    Created based project. Added to scenes as examples.
    One scene dedicated to stickman movement/animation (WASD and arrows to move, E to sit and ESC to stand up).
    The other scene is to make the typewriter effect over a text.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody2D;
+    public Animator animator;
+    public float speed = 5f;
+
+    private Vector2 movement;
+    private Vector2 velocity;
+    private bool sitting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!sitting)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("SitDown");
+                sitting = true;
+            }
+        }
+        else if (sitting && Input.GetKeyDown(KeyCode.Escape))
+        {
+            animator.SetTrigger("StandUp");
+            sitting = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody2D.MovePosition(rigidBody2D.position + movement * (speed * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public Text textField;
+    private float delta = 0f;
+    private float deltaPerChar = 0.4f;
+    private int index = 0;
+    private string text = "Mega testo super rilevante che cambia le sorti di ogni partita univocamente distinta.";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (index < text.Length)
+        {
+            delta += Time.fixedDeltaTime;
+            if (delta >= deltaPerChar)
+            {
+                delta -= deltaPerChar;
+                index++;
+                textField.text = text.Substring(0, index) + "<color=#00000000>" + text.Substring(index) + "</color>";
+            }
+        }
+        
+    }
+}
